bbf942288bd8e3dbcb417340dc3824c4735388c1
Bryan Gonzalez <72216201+BryanGonA@users.noreply.github.com>
2026-01-12 22:23:08 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://bryangona.github.io',
-  base: '/360event', 
+  base: '/360event',
+  build: {
+    format: 'directory', // Esto genera carpeta/index.html en lugar de archivo.html
+  }, 
 });
